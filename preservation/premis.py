3893527b596755b6cac97dce182e3ade601dcdcb
Mikko Vatanen <mikko.vatanen@csc.fi>
2015-06-05 17:48:28 +0300

    KDKPAS-922 Move PREMIS generation code to preservation.premis and preservation.xmlutil modules

--- preservation/premis.py
+++ preservation/premis.py
@@ -1,7 +1,471 @@
-"""Premis serialization from / to JSON and XML"""
+"""Functions for reading and generating PREMIS Data Dictionaries as
+xml.etree.ElementTree data structures.
+
+References:
+
+    * PREMIS http://www.loc.gov/standards/premis/
+    * ElementTree
+    https://docs.python.org/2.6/library/xml.etree.elementtree.html
+
+"""
+
 
 import json
 
+import xml.etree.ElementTree as ET
+
+import preservation.xmlutil
+
+PREMIS_NS = 'info:lc/xmlns/premis-v2'
+XSI_NS = 'http://www.w3.org/2001/XMLSchema-instance'
+
+
+def serialize(root_element):
+    """Serialize ElementTree structure with PREMIS namespace mapping.
+
+    This modifies the default "ns0:tag" style prefixes to "premis:tag"
+    prefixes.
+
+    :element: Starting element to serialize
+    :returns: Serialized XML as string
+
+    """
+
+    def register_namespace(prefix, uri):
+        """foo"""
+        ns_map = getattr(ET, '_namespace_map')
+        ns_map[uri] = prefix
+
+    register_namespace('premis', PREMIS_NS)
+    register_namespace('xsi', XSI_NS)
+
+    preservation.xmlutil.indent(root_element)
+
+    return ET.tostring(root_element)
+
+
+def premis_ns(tag, prefix=""):
+    """Prefix ElementTree tags with PREMIS namespace.
+
+    object -> {info:lc...premis}object
+
+    :tag: Tag name as string
+    :returns: Prefixed tag
+
+    """
+    if prefix:
+        tag = tag[0].upper() + tag[1:]
+        return '{%s}%s%s' % (PREMIS_NS, prefix, tag)
+    return '{%s}%s' % (PREMIS_NS, tag)
+
+
+def xsi_ns(tag):
+    """Prefix ElementTree tags with XSI namespace.
+
+    object -> {info:lc...premis}object
+
+    :tag: Tag name as string
+    :returns: Prefixed tag
+
+    """
+    return '{%s}%s' % (XSI_NS, tag)
+
+
+def _element(tag, prefix=""):
+    """Return _ElementInterface with PREMIS namespace.
+
+    Prefix parameter is useful for adding prefixed to lower case tags. It just
+    uppercases first letter of tag and appends it to prefix::
+
+        element = _element('objectIdentifier', 'linking')
+        element.tag
+        'linkingObjectIdentifier'
+
+    :tag: Tagname
+    :prefix: Prefix for the tag (default="")
+    :returns: ElementTree element object
+
+    """
+    return ET.Element(premis_ns(tag, prefix))
+
+
+def _subelement(parent, tag, prefix=""):
+    """Return subelement for the given parent element. Created element is
+    appelded to parent element.
+
+    :parent: Parent element
+    :tag: Element tagname
+    :prefix: Prefix for the tag
+    :returns: Created subelement
+
+    """
+    return ET.SubElement(parent, premis_ns(tag, prefix))
+
+
+def premis_identifier(identifier_type, identifier_value, prefix=""):
+    """Return PREMIS identifier segments.
+
+    Produces without prefix the following PREMIS segment::
+
+
+          <premis:objectIdentifier>
+              <premis:objectIdentifierType>
+                  preservation-sig-id
+              </premis:objectIdentifierType>
+              <premis:objectIdentifierValue>
+                  preservation-sig-c8b978b6-e160-4497-8027-e19fa0297766
+              </premis:objectIdentifierValue>
+          </premis:objectIdentifier>
+
+    With prefix='relatedObject' the following PREMIS segment::
+
+          <premis:relatedObjectIdentification>
+              <premis:relatedObjectIdentifierType>
+                  preservation-sip-id
+              </premis:relatedObjectIdentifierType>
+              <premis:relatedObjectIdentifierValue>
+                  preservation-sip-1ac641ec-223f-42f4-86c2-9402451d63bf
+              </premis:relatedObjectIdentifierValue>
+          </premis:relatedObjectIdentification>
+
+    With prefix='dependency' the following PREMIS segment::
+
+        <premis:dependencyIdentifier>
+            <premis:dependencyIdentifierType>
+                local</premis:dependencyIdentifierType>
+            <premis:dependencyIdentifierValue>
+                kdk-sip-premis-object001</premis:dependencyIdentifierValue>
+        </premis:dependencyIdentifier>
+
+    With prefix='linking' the following PREMIS segment::
+
+        <premis:linkingObjectIdentifier>
+            <premis:linkingObjectIdentifierType>
+                preservation-sip-id</premis:linkingObjectIdentifierType>
+            <premis:linkingObjectIdentifierValue>
+                preservation-sip-1ac641ec</premis:linkingObjectIdentifierValue>
+        </premis:linkingObjectIdentifier>
+
+    """
+
+    if not prefix:
+        prefix = 'object'
+
+    if prefix == 'relatedObject':
+        _identifier = _element('Identification', prefix)
+    else:
+        _identifier = _element('Identifier', prefix)
+
+    _value = _subelement(_identifier, 'IdentifierType', prefix)
+    _value.text = identifier_type
+
+    _type = _subelement(_identifier, 'IdentifierValue', prefix)
+    _type.text = identifier_value
+
+    return _identifier
+
+
+def get_identifier_type_value(object_or_identifier):
+    """Return identifierType and IdentifierValue from given PREMIS identifier
+    or object. If segment contains multiple identifiers, returns first
+    occurrence.
+
+    :object_or_identifier: TODO
+    :returns: (identifier_type, identifier_value)
+
+    """
+
+    identifier = object_or_identifier
+
+    if identifier.find(premis_ns('objectIdentifier')) is not None:
+        identifier = identifier.find(premis_ns('objectIdentifier'))
+
+    if identifier.find(premis_ns('relatedObjectIdentification')) is not None:
+        identifier = identifier.find(premis_ns('relatedObjectIdentification'))
+
+    return (
+        identifier.find(premis_ns('objectIdentifierType')).text,
+        identifier.find(premis_ns('objectIdentifierValue')).text)
+
+
+def premis_relationship(
+        relationship_type, relationship_subtype,
+        related_object):
+
+    """TODO: Docstring for premis_relationshi.
+
+    :relationship_type: TODO
+    :subtype: TODO
+    :related_object_identification: TODO
+    :returns: TODO
+
+    Produces the following PREMIS segment::
+
+      <premis:relationship>
+
+          <premis:relationshipType>structural</premis:relationshipType>
+          <premis:relationshipSubType>
+              is included in
+          </premis:relationshipSubType>
+
+          {{ premis_identifier(prefix=related) }}
+
+      </premis:relationship>
+
+    """
+
+    relationship = _element('relationship')
+
+    _type = _subelement(relationship, 'relationshipType')
+    _type.text = relationship_type
+
+    _subtype = _subelement(relationship, 'relationshipSubType')
+    _subtype.text = relationship_subtype
+
+    (related_type, related_value) = get_identifier_type_value(
+        related_object)
+
+    related_identifier = premis_identifier(
+        related_type, related_value, prefix='relatedObject')
+
+    relationship.append(related_identifier)
+
+    return relationship
+
+
+def premis_environment(dependency_identifier=None):
+    """Return the PREMIS environment structure.
+
+    :dependency_identifier: PREMIS identifier structure
+    :returns: None
+
+    Returns the following ElementTree structure::
+
+        <premis:environment>
+            <premis:dependency>
+
+                {{ dependency_identifier }}
+
+            </premis:dependency>
+        </premis:environment>
+
+    """
+
+    environment = _element('environment')
+
+    if dependency_identifier:
+        dependency = _subelement(environment, 'dependency')
+        dependency.append(dependency_identifier)
+
+    return environment
+
+
+def premis_object(
+        identifier,
+        original_name,
+        child_elements=None,
+        representation=False):
+
+    """Return the PREMIS object.
+
+        :identifier: PREMIS identifier
+        :original_name: Original name field
+        :child_elements=None: Any other element appended
+        :representation=False:
+
+    Returns the following ElementTree structure::
+
+        <premis:object xsi:type="premis:representation">
+
+            {{ premis_identifier() }}
+
+            <premis:originalName>varmiste.sig</premis:originalName>
+
+            {{ premis_relationship() }}
+
+        </premis:object>
+
+    """
+
+    _object = _element('object')
+
+    _object.append(identifier)
+
+    if representation:
+        _object.set(xsi_ns('type'), 'premis:representation')
+
+    _original_name = _subelement(_object, 'originalName')
+    _original_name.text = original_name
+
+    if child_elements:
+        for element in child_elements:
+            _object.append(element)
+
+    return _object
+
+
+def premis_agent(
+        identifier, agent_name, agent_type):
+    """Returns PREMIS agent element
+
+    :identifier: PREMIS identifier for the agent
+    :agent_name: Agent name
+    :agent_type: Agent type
+
+    Returns the following ElementTree structure::
+
+        <premis:agent>
+            <premis:agentIdentifier>
+                <premis:agentIdentifierType>
+                    preservation-agent-id</premis:agentIdentifierType>
+                <premis:agentIdentifierValue>
+                    preservation-agent-check_virus_clamscan.py-0.63-1422
+                </premis:agentIdentifierValue>
+            </premis:agentIdentifier>
+            <premis:agentName>check_virus_clamscan.py</premis:agentName>
+            <premis:agentType>software</premis:agentType>
+        </premis:agent>
+
+    """
+
+    agent = _element('agent')
+
+    agent.append(identifier)
+
+    _agent_name = _subelement(agent, 'agentName')
+    _agent_name.text = agent_name
+
+    _agent_type = _subelement(agent, 'agentType')
+    _agent_type.text = agent_type
+
+    return agent
+
+
+def premis_premis(child_elements=None):
+    """Create PREMIS Data Dictionary root element.
+
+    :child_elements: Any elements appended to the PREMIS dictionary
+
+    Returns the following ElementTree structure::
+
+
+        <premis:premis
+            xmlns:premis="info:lc/xmlns/premis-v2"
+            xmlns:xsi="http://www.w3.org/2001/xmlschema-instance"
+            xsi:schemalocation="info:lc/xmlns/premis-v2
+                                http://www.loc.gov/standards/premis/premis.xsd"
+            version="2.2">
+
+    """
+    _premis = _element('premis')
+    _premis.set(
+        xsi_ns('schemaLocation'),
+        'info:lc/xmlns/premis-v2 '
+        'http://www.loc.gov/standards/premis/premis.xsd')
+
+    _premis.set('version', '2.2')
+
+    if child_elements:
+        for element in child_elements:
+            _premis.append(element)
+
+    return _premis
+
+
+def premis_event_outcome(outcome, detail_note):
+    """Create PREMIS event outcome DOM structure.
+
+    :outcome: Event outcome (success, failure)
+    :detail_note: Description for the event outcome
+
+    Returns the following ElementTree structure::
+
+        <premis:eventOutcomeInformation>
+            <premis:eventOutcome>success</premis:eventOutcome>
+            <premis:eventOutcomeDetail>
+                <premis:eventOutcomeDetailNote>
+                    mets.xml sha1 4d0c38dedcb5e5fc93586cfa2b7ebedbd63 OK
+                </premis:eventOutcomeDetailNote>
+            </premis:eventOutcomeDetail>
+        </premis:eventOutcomeInformation>
+
+
+    """
+
+    outcome_information = _element('eventOutcomeInformation')
+
+    _outcome = _subelement(outcome_information, 'eventOutcome')
+    _outcome.text = outcome
+
+    detail = _subelement(outcome_information, 'eventOutcomeDetail')
+
+    _detail_note = _subelement(detail, 'eventOutcomeDetailNote')
+    _detail_note.text = detail_note
+
+    return outcome_information
+
+
+def premis_event(
+        identifier, event_type, event_date_time, event_detail,
+        child_elements=None, linking_objects=None):
+    """Create PREMIS event element.
+
+    :identifier: PREMIS event identifier
+    :event_type: Type for the event
+    :event_date_time: Event time
+    :event_detail: Event details
+    :child_elements: Any child elements appended to the event (default=None)
+    :linking_objects: Any linking objects appended to the event (default=None)
+
+    Returns the following ElementTree structure::
+
+        <premis:event>
+
+            <premis:eventType>digital signature validation</premis:eventType>
+            <premis:eventDateTime>2015-02-03T13:04:25</premis:eventDateTime>
+            <premis:eventDetail>
+                Submission information package digital signature validation
+            </premis:eventDetail>
+
+            {{ child elements }}
+
+        </premis:event>
+
+
+    """
+
+    event = _element('event')
+
+    event.append(identifier)
+
+    _event_type = _subelement(event, 'eventType')
+    _event_type.text = event_type
+
+    _event_date_time = _subelement(event, 'eventDateTime')
+    _event_date_time.text = event_date_time
+
+    _event_detail = _subelement(event, 'eventDetail')
+    _event_detail.text = event_detail
+
+    if child_elements:
+        for element in child_elements:
+            event.append(element)
+
+    if linking_objects:
+        for _object in linking_objects:
+            linking_object = premis_identifier(
+                _object.findtext('.//' + premis_ns('objectIdentifierType')),
+                _object.findtext('.//' + premis_ns('objectIdentifierValue')),
+                'linkingObject')
+            event.append(linking_object)
+
+    return event
+
+
+#
+# TODO: Remove JSON Event class after refactoring Premis generation to
+# ElementTree factories
+#
 
 class Event(object):
 
--- tests/premis_test.py
+++ tests/premis_test.py
@@ -1,422 +1,7 @@
 """Test for the Premis class"""
 
+import preservation.premis as p
 import xml.etree.ElementTree as ET
-
-PREMIS_NS = 'info:lc/xmlns/premis-v2'
-XSI_NS = 'http://www.w3.org/2001/XMLSchema-instance'
-
-
-def premis_ns(tag, prefix=""):
-    """TODO: Docstring for premis.
-
-    :tag: TODO
-    :returns: TODO
-
-    """
-    if prefix:
-        tag = tag[0].upper() + tag[1:]
-        return '{%s}%s%s' % (PREMIS_NS, prefix, tag)
-    return '{%s}%s' % (PREMIS_NS, tag)
-
-
-def _element(tag, prefix=""):
-    """TODO: Docstring for _element.
-
-    :element: TODO
-    :returns: TODO
-
-    """
-    return ET.Element(premis_ns(tag, prefix))
-
-
-def _subelement(parent, tag, prefix=""):
-    """TODO: Docstring for _element.
-
-    :element: TODO
-    :returns: TODO
-
-    """
-    return ET.SubElement(parent, premis_ns(tag, prefix))
-
-
-def xsi_ns(tag):
-    """TODO: Docstring for premis.
-
-    :tag: TODO
-    :returns: TODO
-
-    """
-    return '{%s}%s' % (XSI_NS, tag)
-
-
-def premis_identifier(identifier_type, identifier_value, prefix=""):
-    """Return PREMIS identifier segments.
-
-    Produces without prefix the following PREMIS segment::
-
-
-          <premis:objectIdentifier>
-              <premis:objectIdentifierType>
-                  preservation-sig-id
-              </premis:objectIdentifierType>
-              <premis:objectIdentifierValue>
-                  preservation-sig-c8b978b6-e160-4497-8027-e19fa0297766
-              </premis:objectIdentifierValue>
-          </premis:objectIdentifier>
-
-    With prefix='relatedObject' the following PREMIS segment::
-
-          <premis:relatedObjectIdentification>
-              <premis:relatedObjectIdentifierType>
-                  preservation-sip-id
-              </premis:relatedObjectIdentifierType>
-              <premis:relatedObjectIdentifierValue>
-                  preservation-sip-1ac641ec-223f-42f4-86c2-9402451d63bf
-              </premis:relatedObjectIdentifierValue>
-          </premis:relatedObjectIdentification>
-
-    With prefix='dependency' the following PREMIS segment::
-
-        <premis:dependencyIdentifier>
-            <premis:dependencyIdentifierType>
-                local</premis:dependencyIdentifierType>
-            <premis:dependencyIdentifierValue>
-                kdk-sip-premis-object001</premis:dependencyIdentifierValue>
-        </premis:dependencyIdentifier>
-
-    With prefix='linking' the following PREMIS segment::
-
-        <premis:linkingObjectIdentifier>
-            <premis:linkingObjectIdentifierType>
-                preservation-sip-id</premis:linkingObjectIdentifierType>
-            <premis:linkingObjectIdentifierValue>
-                preservation-sip-1ac641ec</premis:linkingObjectIdentifierValue>
-        </premis:linkingObjectIdentifier>
-
-    """
-
-    if not prefix:
-        prefix = 'object'
-
-    if prefix == 'relatedObject':
-        _identifier = _element('Identification', prefix)
-    else:
-        _identifier = _element('Identifier', prefix)
-
-    _value = _subelement(_identifier, 'IdentifierType', prefix)
-    _value.text = identifier_type
-
-    _type = _subelement(_identifier, 'IdentifierValue', prefix)
-    _type.text = identifier_value
-
-    return _identifier
-
-
-def get_identifier_type_value(object_or_identifier):
-    """Return identifierType and IdentifierValue from given PREMIS identifier
-    or object. If segment contains multiple identifiers, returns first
-    occurrence.
-
-    :object_or_identifier: TODO
-    :returns: (identifier_type, identifier_value)
-
-    """
-
-    identifier = object_or_identifier
-
-    if identifier.find(premis_ns('objectIdentifier')) is not None:
-        identifier = identifier.find(premis_ns('objectIdentifier'))
-
-    if identifier.find(premis_ns('relatedObjectIdentification')) is not None:
-        identifier = identifier.find(premis_ns('relatedObjectIdentification'))
-
-    return (
-        identifier.find(premis_ns('objectIdentifierType')).text,
-        identifier.find(premis_ns('objectIdentifierValue')).text)
-
-
-def premis_relationship(
-        relationship_type, relationship_subtype,
-        related_object):
-
-    """TODO: Docstring for premis_relationshi.
-
-    :relationship_type: TODO
-    :subtype: TODO
-    :related_object_identification: TODO
-    :returns: TODO
-
-    Produces the following PREMIS segment::
-
-      <premis:relationship>
-
-          <premis:relationshipType>structural</premis:relationshipType>
-          <premis:relationshipSubType>
-              is included in
-          </premis:relationshipSubType>
-
-          {{ premis_identifier(prefix=related) }}
-
-      </premis:relationship>
-
-    """
-
-    relationship = _element('relationship')
-
-    _type = _subelement(relationship, 'relationshipType')
-    _type.text = relationship_type
-
-    _subtype = _subelement(relationship, 'relationshipSubType')
-    _subtype.text = relationship_subtype
-
-    (related_type, related_value) = get_identifier_type_value(
-        related_object)
-
-    related_identifier = premis_identifier(
-        related_type, related_value, prefix='relatedObject')
-
-    relationship.append(related_identifier)
-
-    return relationship
-
-
-def premis_environment(dependency_identifier=None):
-    """TODO: Docstring for premis_environment.
-
-    :dependency: TODO
-    :returns: TODO
-
-    Produces the following PREMIS segment::
-
-        <premis:environment>
-            <premis:dependency>
-
-                {{ dependency_identifier }}
-
-            </premis:dependency>
-        </premis:environment>
-
-    """
-
-    environment = _element('environment')
-
-    if dependency_identifier:
-        dependency = _subelement(environment, 'dependency')
-        dependency.append(dependency_identifier)
-
-    return environment
-
-
-def premis_object(
-        identifier,
-        original_name,
-        child_elements=None,
-        representation=False):
-
-    """TODO: Docstring for Event.
-
-    :fields: TODO
-    :returns: TODO
-
-        <premis:object xsi:type="premis:representation">
-
-            {{ premis_identifier() }}
-
-            <premis:originalName>varmiste.sig</premis:originalName>
-
-            {{ premis_relationship() }}
-
-        </premis:object>
-
-    """
-
-    _object = _element('object')
-
-    _object.append(identifier)
-
-    if representation:
-        _object.set(xsi_ns('type'), 'premis:representation')
-
-    _original_name = _subelement(_object, 'originalName')
-    _original_name.text = original_name
-
-    if child_elements:
-        for element in child_elements:
-            _object.append(element)
-
-    return _object
-
-
-def premis_agent(
-        identifier, agent_name, agent_type):
-    """Returns PREMIS agent element
-
-    Generates the following PREMIS segment::
-
-        <premis:agent>
-            <premis:agentIdentifier>
-                <premis:agentIdentifierType>
-                    preservation-agent-id</premis:agentIdentifierType>
-                <premis:agentIdentifierValue>
-                    preservation-agent-check_virus_clamscan.py-0.63-1422
-                </premis:agentIdentifierValue>
-            </premis:agentIdentifier>
-            <premis:agentName>check_virus_clamscan.py</premis:agentName>
-            <premis:agentType>software</premis:agentType>
-        </premis:agent>
-
-    """
-
-    agent = _element('agent')
-
-    agent.append(identifier)
-
-    _agent_name = _subelement(agent, 'agentName')
-    _agent_name.text = agent_name
-
-    _agent_type = _subelement(agent, 'agentType')
-    _agent_type.text = agent_type
-
-    return agent
-
-
-def premis_premis(child_elements=None):
-    """TODO: Docstring for premis_premis.
-    :returns: TODO
-
-    """
-    _premis = _element('premis')
-    _premis.set(
-        xsi_ns('schemaLocation'),
-        'info:lc/xmlns/premis-v2 '
-        'http://www.loc.gov/standards/premis/premis.xsd')
-
-    _premis.set('version', '2.2')
-
-    if child_elements:
-        for element in child_elements:
-            _premis.append(element)
-    return _premis
-
-
-def premis_event_outcome(outcome, detail_note):
-    """TODO: Docstring for premis_event_outcome_information.
-
-    :arg1: TODO
-    :returns: TODO
-
-        <premis:eventOutcomeInformation>
-            <premis:eventOutcome>success</premis:eventOutcome>
-            <premis:eventOutcomeDetail>
-                <premis:eventOutcomeDetailNote>
-                    mets.xml sha1 4d0c38dedcb5e5fc93586cfa2b7ebedbd63 OK
-                </premis:eventOutcomeDetailNote>
-            </premis:eventOutcomeDetail>
-        </premis:eventOutcomeInformation>
-
-
-    """
-
-    outcome_information = _element('eventOutcomeInformation')
-
-    _outcome = _subelement(outcome_information, 'eventOutcome')
-    _outcome.text = outcome
-
-    detail = _subelement(outcome_information, 'eventOutcomeDetail')
-
-    _detail_note = _subelement(detail, 'eventOutcomeDetailNote')
-    _detail_note.text = detail_note
-
-    return outcome_information
-
-
-def premis_event(
-        identifier, event_type, event_date_time, event_detail,
-        child_elements=None, linking_objects=None):
-    """TODO: Docstring for premis_event.
-
-    :arg1: TODO
-    :returns: TODO
-
-
-        <premis:event>
-
-            <premis:eventType>digital signature validation</premis:eventType>
-            <premis:eventDateTime>2015-02-03T13:04:25</premis:eventDateTime>
-            <premis:eventDetail>
-                Submission information package digital signature validation
-            </premis:eventDetail>
-
-            {{ child elements }}
-
-        </premis:event>
-
-
-    """
-
-    event = _element('event')
-
-    event.append(identifier)
-
-    _event_type = _subelement(event, 'eventType')
-    _event_type.text = event_type
-
-    _event_date_time = _subelement(event, 'eventDateTime')
-    _event_date_time.text = event_date_time
-
-    _event_detail = _subelement(event, 'eventDetail')
-    _event_detail.text = event_detail
-
-    if child_elements:
-        for element in child_elements:
-            event.append(element)
-
-    if linking_objects:
-        for _object in linking_objects:
-            linking_object = premis_identifier(
-                _object.findtext('.//' + premis_ns('objectIdentifierType')),
-                _object.findtext('.//' + premis_ns('objectIdentifierValue')),
-                'linkingObject')
-            event.append(linking_object)
-
-    return event
-
-
-def indent(elem, level=0):
-    """Indent the elementtree"""
-    i = "\n" + level*"  "
-    if len(elem):
-        if not elem.text or not elem.text.strip():
-            elem.text = i + "  "
-        if not elem.tail or not elem.tail.strip():
-            elem.tail = i
-        for elem in elem:
-            indent(elem, level+1)
-        if not elem.tail or not elem.tail.strip():
-            elem.tail = i
-    else:
-        if level and (not elem.tail or not elem.tail.strip()):
-            elem.tail = i
-
-
-def serialize(element):
-    """TODO: Docstring for serialize.
-
-    :element: TODO
-    :returns: TODO
-
-    """
-
-    def register_namespace(prefix, uri):
-        """foo"""
-        ns_map = getattr(ET, '_namespace_map')
-        ns_map[uri] = prefix
-
-    register_namespace('premis', PREMIS_NS)
-    register_namespace('xsi', XSI_NS)
-
-    indent(element)
-    return ET.tostring(element)
 
 
 def test_premis_ns():
@@ -427,26 +12,26 @@ def test_premis_ns():
 
     # SIP & METS object identifiers
 
-    sip_identifier = premis_identifier('preservation-sip-id', 'csc-sip-001')
-    mets_identifier = premis_identifier('METS-OBJID', 'mets-objid-999')
+    sip_identifier = p.premis_identifier('preservation-sip-id', 'csc-sip-001')
+    mets_identifier = p.premis_identifier('METS-OBJID', 'mets-objid-999')
 
     # PREMIS relationship
 
-    relationship = premis_relationship(
+    relationship = p.premis_relationship(
         relationship_type='structural',
         relationship_subtype='is included in',
         related_object=mets_identifier)
 
     # PREMIS enviroment
 
-    dependency_identifier = premis_identifier(
+    dependency_identifier = p.premis_identifier(
         'local', 'kdk-sip-premis-object001', 'dependency')
 
-    environment = premis_environment(dependency_identifier)
+    environment = p.premis_environment(dependency_identifier)
 
     # PREMIS object
 
-    sip_object = premis_object(
+    sip_object = p.premis_object(
         original_name='csc-sip-001.zip',
         identifier=sip_identifier,
         child_elements=[relationship, environment],
@@ -454,13 +39,13 @@ def test_premis_ns():
 
     # Create the PREMIS event
 
-    event_identifier = premis_identifier(
+    event_identifier = p.premis_identifier(
         'preservation-event-id', 'event-id-1234', 'event')
 
-    event_outcome = premis_event_outcome(
+    event_outcome = p.premis_event_outcome(
         'success', 'Validation successful - OK')
 
-    event = premis_event(
+    event = p.premis_event(
         event_identifier,
         'digital signature validation',
         '1.1.2015',
@@ -469,11 +54,11 @@ def test_premis_ns():
 
     # PREMIS XML & print
 
-    premis = premis_premis([sip_object, event])
-    print serialize(premis)
+    premis = p.premis_premis([sip_object, event])
+    print p.serialize(premis)
 
-    xml = serialize(premis)
+    xml = p.serialize(premis)
 
-    premis = ET.fromstring(xml)
+    premis_root = ET.fromstring(xml)
 
-    print serialize(premis)
+    print p.serialize(premis_root)

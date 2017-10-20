37e632551054f5d06ae0947d58a1e96ea1f64441
Juha Lehtonen <juha.lehtonen@csc.fi>
2017-10-20 17:18:08 +0300

    KDKPAS-1433 Tests for premis agents

--- tests/agent_test.py
+++ tests/agent_test.py
@@ -19,15 +19,54 @@ def test_agent():
 
 def test_iter_agents():
     """Test iter_agents"""
-    # TODO
+    agent1 = a.agent(p.identifier('a', 'b', 'agent'), 'nimi1' 'tyyppi')
+    agent2 = a.agent(p.identifier('a', 'b', 'agent'), 'nimi2' 'tyyppi')
+    agent3 = a.agent(p.identifier('a', 'b', 'agent'), 'nimi3' 'tyyppi')
+    premisroot = p.premis(child_elements=[agent1, agent2, agent3])
+    agentlist = []
+    i = 0
+    for _agent in a.iter_agents(premisroot):
+        agentlist.append(_agent)
+        i = i + 1
+        assert a.parse_name(_agent) == 'nimi'+str(i)
+    assert len(agentlist) == 3
 
 
 def test_agent_count():
     """Test agent_count"""
-    # TODO
+    agent1 = a.agent(p.identifier('a', 'b', 'agent'), 'nimi' 'tyyppi1')
+    agent2 = a.agent(p.identifier('a', 'b', 'agent'), 'nimi' 'tyyppi2')
+    agent3 = a.agent(p.identifier('a', 'b', 'agent'), 'nimi' 'tyyppi1')
+    premisroot = p.premis(child_elements=[agent1, agent2, agent3])
+    assert agent_count(premisroot) == 3
 
 
 def test_agents_with_type():
     """Test agents_with_type"""
-    # TODO
+    agent1 = a.agent(p.identifier('a', 'b', 'agent'), 'nimi' 'tyyppi1')
+    agent2 = a.agent(p.identifier('a', 'b', 'agent'), 'nimi' 'tyyppi2')
+    agent3 = a.agent(p.identifier('a', 'b', 'agent'), 'nimi' 'tyyppi1')
+    agentlist = []
+    for _agent in a.agents_with_type([agent1, agent2, agent3], 'tyyppi1'):
+        agentlist.append(_agent)
+        assert a.parse_type(_agent) == 'tyyppi1'
+    assert len(agentlist) == 2
+
+
+def test_parse_name():
+    """Test parse_name"""
+    agent = a.agent(p.identifier('a', 'b', 'agent'), 'nimi' 'tyyppi', note='nootti')
+    assert a.parse_name(agent) == 'nimi'
+
+
+def test_parse_type():
+    """Test parse_type"""
+    agent = a.agent(p.identifier('a', 'b', 'agent'), 'nimi' 'tyyppi', note='nootti')
+    assert a.parse_type(agent) == 'tyyppi'
+
+
+def test_parse_note():
+    """Test parse_note"""
+    agent = a.agent(p.identifier('a', 'b', 'agent'), 'nimi' 'tyyppi', note='nootti')
+    assert a.parse_note(agent) == 'nootti'
 

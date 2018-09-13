2cca2158a4c6c2f92d5ebaf63e888e1bd48042b7
Heikki Mikkolainen <heikki.mikkolainen@csc.fi>
2018-09-13 10:14:06 +0300

    Add requirements to setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
-"""
-Install premis
-"""
+"""Install premis """
 
-import os
 from setuptools import setup, find_packages
 
 
@@ -11,7 +8,13 @@ def main():
     setup(
         name='premis',
         packages=find_packages(exclude=['tests', 'tests.*']),
-        version='0.1')
+        version='0.1',
+        install_requires=['lxml', 'xml-helpers'],
+        dependency_links=[
+            'git+https://gitlab.csc.fi/dpres/xml-helpers.git'
+            '@develop#egg=xml-helpers-0.0'
+        ]
+    )
 
 
 if __name__ == '__main__':

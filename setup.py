71d2e0b8fbf7d6096cdd6e612316adda42756064
Heikki Mikkolainen <heikki.mikkolainen@csc.fi>
2019-01-31 16:06:07 +0200

    TPASPKT-211 Use PEP 508 URL dependencies instead of dependency links

--- setup.py
+++ setup.py
@@ -10,10 +10,10 @@ def main():
         name='premis',
         packages=find_packages(exclude=['tests', 'tests.*']),
         version=get_version(),
-        install_requires=['lxml', 'xml-helpers'],
-        dependency_links=[
-            'git+https://gitlab.csc.fi/dpres/xml-helpers.git'
-            '@develop#egg=xml-helpers-0.0'
+        install_requires=[
+            'lxml',
+            'xml-helpers@git+https://gitlab.csc.fi/dpres/xml-helpers.git'
+            '@develop'
         ]
     )
 

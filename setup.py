73d2fe82d9f66bb5f53e3a98ec7621b042909285
Janne Pulkkinen <janne.pulkkinen@csc.fi>
2019-06-05 11:57:42 +0300

    KDKPAS-2010 Python 3 compatibility

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ def main():
     setup(
         name='premis',
         packages=find_packages(exclude=['tests', 'tests.*']),
+        include_package_data=True,
         version=get_version(),
         install_requires=[
             'lxml',
